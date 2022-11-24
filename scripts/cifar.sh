dfd1f77f6c99398952ba4850242752e0c9181047
YoungRaeKimm <nahappy15@naver.com>
2022-11-24 15:23:02 +0900

    fix typo

--- scripts/cifar.sh
+++ scripts/cifar.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 py3clean ./
-CUDA_VISIBLE_DEVICES=0 python -B -m run.py \
+CUDA_VISIBLE_DEVICES=0 python -B ../run.py \
                         --ILtype task \
                         --dataset cifar100 \
                         --split 10 \
-                        --LRS False \
+                        --alpha 0.5 \
+                        --beta 0.5 \
+                        --gamma 0.5 \
                         --memory_size 200 \
                         -everytest False 
